e145f5fdc7685e3e43d608a982d70e12248fd0eb
Fabio Diniz Pelacio <fabiodinizpelacio@Mac.localdomain>
2026-01-19 00:52:57 -0800

    Remove test pages

--- app/test/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Test() {
-  return <main style={{ padding: 40, fontFamily: "system-ui, Arial" }}>TEST PAGE</main>;
-}
--- app/w/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function W() {
-  return <main style={{padding:40}}>W INDEX</main>;
-}
